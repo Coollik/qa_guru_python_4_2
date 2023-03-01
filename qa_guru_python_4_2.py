82b5a5592ad4afb54ba561db6b6dbdf700e6a471
Alexis <kulikov.alx@yandex.ru>
2023-03-01 23:50:57 +0400

    вынес фикстуру в conftest.py

--- qa_guru_python_4_2.py
+++ qa_guru_python_4_2.py
@@ -2,13 +2,10 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-@pytest.fixture
-def browser_open():
-    browser.open('https://google.com')
-
-@pytest.fixture()
-def type_value():
+def test_selene(browser_open):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_selene(browser_open, type_value):
+def test_failed(browser_open):
+    browser.element('[name="q"]').should(be.blank).type('ratamahata').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))

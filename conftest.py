fd69d11e2e99b2cda6b4aee20a72dcea4129c5a6
Alexis <kulikov.alx@yandex.ru>
2023-03-02 00:03:51 +0400

    Не помню, что натворил

--- conftest.py
+++ conftest.py
@@ -2,6 +2,14 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-@pytest.fixture
+
+@pytest.fixture(scope="function") # Со скоупом "function" фикстура будет заново запускаться при каждом вызове функции "clear_field"
+def clear_field():
+    yield
+    browser.element('[name="q"]').clear()
+
+@pytest.fixture(scope="session")  # Со скоупом "session" фикстура запустится один раз на всю сессию при вызове функции "browser_open"
 def browser_open():
+    browser.config.window_height = 640
+    browser.config.window_width = 480
     browser.open('https://google.com')
--- qa_guru_python_4_2.py
+++ qa_guru_python_4_2.py
@@ -2,10 +2,12 @@ import pytest
 from selene.support.shared import browser
 from selene import be, have
 
-def test_selene(browser_open):
+
+def test_selene(browser_open, clear_field):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_failed(browser_open):
+
+def test_failed(clear_field):
     browser.element('[name="q"]').should(be.blank).type('ratamahata').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))

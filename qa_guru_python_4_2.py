3a7372675e740231a4acd0d9dfb8a7d1ad97b7dd
Alexis <kulikov.alx@yandex.ru>
2023-03-01 23:15:47 +0400

    Описал задание в README.md Создал requirements.txt добавил код в qa_guru_python_4_2.py

--- qa_guru_python_4_2.py
+++ qa_guru_python_4_2.py
@@ -1,7 +1,14 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+@pytest.fixture
+def browser_open():
+    browser.open('https://google.com')
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture()
+def type_value():
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+
+def test_selene(browser_open, type_value):
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
